bf19360037ce6f69f5eeacf4b31b40b2ef887976
Sumit <sphingu@gmail.com>
2021-08-24 00:30:30 +0530

    Added pnpm support and updated several packages

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,12 @@
 import { ApolloServer } from 'apollo-server'
 import { schema } from './schema'
 import { context } from './context'
+import { ApolloServerPluginLandingPageGraphQLPlayground } from 'apollo-server-core';
+ 
 
-export const server = new ApolloServer({ schema, context })
+export const server = new ApolloServer({
+  schema,
+  context,
+  // TODO: make it conditional for production
+  plugins: [ ApolloServerPluginLandingPageGraphQLPlayground() ]
+})
